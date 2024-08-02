35ddb18534fbaa255a4f17d6c7b8c9be1ffde9f6
Akanksha10029 <akanksharani405@gmail.com>
2024-08-02 18:54:58 +0530

    new files added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVariety(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ('Bk', 'Black'),
+        ('Gr', 'Ginger'),
+        ('Kl', 'Kiwi'),
+        ('Ms', 'Masala'),
+        ('Pl', 'Plain'),  
+    ]
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='chai_varieties/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
+    
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import ChaiVariety
 
 # Create your views here.
 def all_app(request):
-    return render(request, 'app/all_app.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'app/all_app.html',{'chais': chais})
 
 def order(request):
     return render(request, 'app/order_app.html')
